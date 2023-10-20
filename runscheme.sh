f6a48cbfe18e24a47e102c86b067b6258644f5b0
Patrick Hammer <patham9@gmail.com>
2023-10-20 23:10:02 +0000

    Update: get rid of newline which is not portable

--- runscheme.sh
+++ runscheme.sh
@@ -1,7 +1,8 @@
 awk '{gsub(/True/, "#t") gsub(/False/, "#f") gsub(/\|\-/, "DERIVE") gsub(/\(\)/, "\047()") gsub(/\(match /, "(Match ") gsub(/\(let /, "(Let ") gsub(/\(let\* /, "(Let\* ") gsub(/\(match /, "(Match ") gsub(/\(case /, "(Case ") gsub(/\(car-atom /, "(car ") gsub(/\(cdr-atom /, "(cdr ")}1' $1 > TRANS.scm
 awk '/;<EVALUATIONS>/ {f=1; next} {if(f) print > "INPUT.scm"; else print > "PROGRAM.scm"}' TRANS.scm
 cat mettamorph.scm PROGRAM.scm > RUN.scm
-echo "\n(! " >> RUN.scm
+echo "" >> RUN.scm
+echo "(! " >> RUN.scm
 cat INPUT.scm >> RUN.scm
 echo ")" >> RUN.scm
 csi -s RUN.scm
