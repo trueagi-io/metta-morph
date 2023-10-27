e0272b52c39efef6690078ccb9b1cdb410ef8b6b
Patrick Hammer <patham9@gmail.com>
2023-10-27 14:00:40 +0200

    Update: roadblock reached showcases by testmacro

--- mettamorph.scm
+++ mettamorph.scm
@@ -24,37 +24,49 @@
     ((_ ((superpose x) ...))
      (amb x ...))
     ((_ arg)
-     (auto-quote-list1 arg))))
+     (auto-quote-list1-with-eval #t arg))))
 
 ;; FUNCTION DEFINITION IN METTA
 
 (define functions (make-hash-table))
 
-(define-syntax =
+(define-syntax testmacro
+  (syntax-rules ()
+    ((_ vari ...)
+     (vari ...))))
+
+(define-syntax =helper
   (syntax-rules ()
     ((_ (name patterni ...) body)
      (begin
        (set! &self (cons '(=def (name patterni ...) body) &self)) ;for = in match
-       (let ((name (match-lambda* ((patterni ...) (auto-quote-list1 body)))))
+       (let ((name (match-lambda* ((patterni ...) (auto-quote-list1-with-eval #t body)))))
             (if (hash-table-exists? functions 'name)
                 (hash-table-set! functions 'name (cons name (hash-table-ref functions 'name)))
                 (hash-table-set! functions 'name (list name))))
        (set! name (lambda args (handle-exceptions exn ((amb-failure-continuation))
                           (apply (amb1 (hash-table-ref functions 'name)) args))))))))
 
+(define-syntax =
+  (syntax-rules (ok)
+    ((_ (ok name patterni ...) body)
+     (=helper (name patterni ...) body))
+    ((_ (name patterni ...) body)
+     (= (ok name (testmacro patterni) ...) body))))
+
 ;; SYNTACTIC CONSTRUCTS FOR DEFINING VARIABLES
 
 (define-syntax Let
   (syntax-rules ()
     ((_ var val body)
-     (match-let* ((var (auto-quote-list1 val))) (auto-quote-list1 body)))))
+     (match-let* ((var (auto-quote-list1-with-eval #t val))) (auto-quote-list1-with-eval #t body)))))
 
 (define-syntax Let*
   (syntax-rules ()
     ((_ ((vari vali) ...) body)
-     (match-let* ((vari (auto-quote-list1 vali)) ...) (auto-quote-list1 body)))
+     (match-let* ((vari (auto-quote-list1-with-eval #t vali)) ...) (auto-quote-list1-with-eval #t body)))
     ((_ (((vari1 vari2) vali) ...) body)
-     (match-let* (((vari1 vari2) (auto-quote-list1 vali)) ...) (auto-quote-list1 body)))))
+     (match-let* (((vari1 vari2) (auto-quote-list1-with-eval #t vali)) ...) (auto-quote-list1-with-eval #t body)))))
 
 ;; SYNTACTIC CONSTRUCTS FOR CHECKING VARIABLES
 
@@ -62,14 +74,14 @@
   (syntax-rules (else)
     ((_ var ((pati bodi) ...))
      (handle-exceptions exn ((amb-failure-continuation))
-                        (match (auto-quote-list1 var) (pati (auto-quote-list1 bodi)) ...)))))
+                        (match (auto-quote-list1-with-eval #t var) (pati (auto-quote-list1-with-eval #t bodi)) ...)))))
 
 (define-syntax If
   (syntax-rules ()
     ((_ condition thenbody elsebody)
-        (if condition (auto-quote-list1 thenbody) (auto-quote-list1 elsebody)))
+        (if condition (auto-quote-list1-with-eval #t thenbody) (auto-quote-list1-with-eval #t elsebody)))
     ((_ condition thenbody)
-        (if condition (auto-quote-list1 thenbody) ((amb-failure-continuation))))))
+        (if condition (auto-quote-list1-with-eval #t thenbody) ((amb-failure-continuation))))))
 
 ;; QUERY STATEMENT EXECUTION OPERATOR
 
@@ -90,7 +102,7 @@
 (define-syntax !
   (syntax-rules ()
     ((_ args ...)
-     (print-solutions (amb-collect (auto-list args ...))))))
+     (print-solutions (amb-collect (auto-quote-list1-with-eval #t (args ...)))))))
 
 ;; AUTO-LIST TO ELIMINATE THE NEED FOR LIST-FUNCTIONCALL DISTINCTION
 
@@ -112,55 +124,57 @@
     ((_ quote then else) then)
     ((_ arg then else) else)))
 
-(define-syntax auto-quote-recursive
- (syntax-rules ()
-    ((_ (argi ...))
-     ((auto-quote argi) ...))
-    ((_ arg)
-     (auto-quote arg))))
-
 (define-syntax auto-quote
  (syntax-rules ()
     ((_ (argi ...))
      (argi ...))
     ((_ arg)
      (handle-exceptions exn (quote arg)
-        (if (or (number? arg) (string? arg) (boolean? arg) (string-prefix? "$" (->string arg)))
+        (if (or (number? arg) (string? arg) (boolean? arg) (string-prefix? "$" (symbol->string 'arg)))
             arg
             (quote arg))))))
 
-(define-syntax auto-list-helper
+(define-syntax auto-list-with-eval-helper
   (syntax-rules ()
-    ((_ expr1 ()) ;empty list
+    ((_ EVAL expr1 ()) ;empty list
      (list expr1))
-    ((_ (expr1i ...) argi ...) ;a nested expression is not a procedure
-     (list (auto-quote-list1 (expr1i ...)) (auto-quote-list1 argi) ...))
-    ((_ expr1 argi ...)
-     (handle-exceptions exn (list (auto-quote-list1 expr1) (auto-quote-list1 argi) ...) (if (procedure? expr1)
-         (apply expr1 (list (auto-quote-list1 argi) ...))
-         (list (auto-quote-list1 expr1) (auto-quote-list1 argi) ...))))))
+    ((_ EVAL (expr1i ...) argi ...) ;a nested expression is not a procedure
+     (list (auto-quote-list1-with-eval EVAL (expr1i ...)) (auto-quote-list1-with-eval EVAL argi) ...))
+    ((_ EVAL expr1 argi ...)
+     (handle-exceptions exn 
+       (if EVAL 
+           (list (auto-quote-list1-with-eval EVAL expr1) (auto-quote-list1-with-eval EVAL argi) ...)
+           ((auto-quote-list1-with-eval EVAL expr1) (auto-quote-list1-with-eval EVAL argi) ...)
+           )
+       (if EVAL
+           (if (procedure? expr1)
+               (apply expr1 (list (auto-quote-list1-with-eval EVAL argi) ...))
+               (list (auto-quote-list1-with-eval EVAL expr1) (auto-quote-list1-with-eval EVAL argi) ...))
+           ((auto-quote-list1-with-eval EVAL expr1) (auto-quote-list1-with-eval EVAL argi) ...))))))
 
-(define-syntax auto-list
+(define-syntax auto-list-with-eval
   (syntax-rules ()
-    ((_ expr)
-     (list (auto-list1 expr)))
-    ((_ expr1 expri ...)
+    ((_ EVAL expr)
+     (list (auto-list1-with-eval EVAL expr)))
+    ((_ EVAL expr1 expri ...)
      (metta-macro-if expr1
        (expr1 expri ...)
-       (auto-list-helper expr1 expri ...)))))
+       (if EVAL
+           (auto-list-with-eval-helper EVAL expr1 expri ...)
+           (expr1 expri ...))))))
 
-(define-syntax auto-list1
+(define-syntax auto-list1-with-eval
    (syntax-rules ()
-    ((_ (vari ...))
-     (auto-list vari ...))
-    ((_ var1)
+    ((_ EVAL (vari ...))
+     (auto-list-with-eval EVAL vari ...))
+    ((_ EVAL var1)
      var1)))
 
-(define-syntax auto-quote-list1
+(define-syntax auto-quote-list1-with-eval
    (syntax-rules ()
-    ((_ (vari ...))
-     (auto-quote (auto-list1 (vari ...))))
-    ((_ var1)
+    ((_ EVAL (vari ...))
+     (auto-quote (auto-list1-with-eval EVAL (vari ...))))
+    ((_ EVAL var1)
      (auto-quote var1))))
 
 ;; EQUALITY
@@ -196,22 +210,22 @@
 (define-syntax add-atom
   (syntax-rules ()
     ((_ space atom)
-     (begin (set! space (cons (auto-quote-list1 atom) space)) '()))))
+     (begin (set! space (cons (auto-quote-list1-with-eval #t atom) space)) '()))))
 
 (define-syntax remove-atom
   (syntax-rules ()
     ((_ space atom)
-     (begin (let ((atm (auto-quote-list1 atom))) (set! space (delete atm space)) '())))))
+     (begin (let ((atm (auto-quote-list1-with-eval #t atom))) (set! space (delete atm space)) '())))))
 
 (define-syntax bind!
   (syntax-rules ()
     ((_ space val)
-     (begin (set! space (auto-quote-list1 val)) '()))))
+     (begin (set! space (auto-quote-list1-with-eval #t val)) '()))))
 
 (define-syntax change-state!
   (syntax-rules ()
     ((_ var val)
-     (begin (set! var (auto-quote-list1 val)) (list 'State val)))))
+     (begin (set! var (auto-quote-list1-with-eval #t val)) (list 'State val)))))
 
 (define-syntax Match
   (syntax-rules (MatchChain)
@@ -221,7 +235,7 @@
      (Match space bind1 (Match space (MatchChain bindi ...) result)))
     ((_ space binds result)
      (handle-exceptions exn ((amb-failure-continuation))
-                        (match-let* ((binds (amb1 space))) (auto-quote-list1 result))))))
+                        (match-let* ((binds (amb1 space))) (auto-quote-list1-with-eval #t result))))))
 
 ;; PROCEDURAL CONSTRUCTS
 
@@ -237,5 +251,5 @@
     ((_ (expri ...))      ;that's why this construct is defined here instead
      (begin
        (set! ret '())
-       (sequential-helper (auto-quote-list1 expri)) ...
+       (sequential-helper (auto-quote-list1-with-eval #t expri)) ...
        (amb1 ret)))))
