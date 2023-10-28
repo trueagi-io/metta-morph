acac86243108567245aae1381e4bbb718a264694
Peter Isaev <pisaev1@gmail.com>
2023-10-28 15:40:51 -0400

    re-org and commenting

--- mettamorph.scm
+++ mettamorph.scm
@@ -5,73 +5,26 @@
 (import (chicken flonum))  ;floating point options
 (import (chicken type))    ;type system
 
-;; COLLAPSE AND SUPERPOSE
 
-(define-syntax collapse
+;; TYPE SYSTEM
+(define-type Atom *)
+(define-type Symbol symbol)
+(define-type Expression list)
+(define-type Bool boolean)
+(define-type Number number)
+(define-type String string)
+
+(define-syntax Typedef
   (syntax-rules ()
-    ((_ args)
-     (filter (lambda (x) (not (== x (if #f 42))))
-             (amb-collect (handle-exceptions exn ((amb-failure-continuation)) args))))))
+    ((_ arg (-> A ... B))
+     (cond-expand
+       (USE_TYPES (: arg (A ... -> B))) (else '())))
+    ((_ arg1 arg2)
+     (cond-expand
+       (USE_TYPES (define-type arg1 arg2)) (else '())))))
 
-(define-syntax superpose
-  (syntax-rules ()
-    ((_ args)
-     (amb1 (superpose-helper args)))))
 
-(define-syntax superpose-helper
-  (syntax-rules ()
-    ((_ ((superpose x) ...))
-     (amb x ...))
-    ((_ arg)
-     (auto-list1 arg))))
-
-;; FUNCTION DEFINITION IN METTA
-
-(define functions (make-hash-table))
-
-(define-syntax =
-  (syntax-rules ()
-    ((_ (name patterni ...) body)
-     (begin
-       (hash-table-set! vars '&self (cons '(=def (name patterni ...) body) (hash-table-ref vars '&self)))
-       (let ((name (match-lambda* ((patterni ...) (auto-list1 body)))))
-            (if (hash-table-exists? functions 'name)
-                (hash-table-set! functions 'name (cons name (hash-table-ref functions 'name)))
-                (hash-table-set! functions 'name (list name))))
-       (set! name (lambda args (handle-exceptions exn ((amb-failure-continuation))
-                          (apply (amb1 (hash-table-ref functions 'name)) args))))))))
-
-;; SYNTACTIC CONSTRUCTS FOR DEFINING VARIABLES
-
-(define-syntax Let
-  (syntax-rules ()
-    ((_ var val body)
-     (match-let* ((var (auto-list1 val))) (auto-list1 body)))))
-
-(define-syntax Let*
-  (syntax-rules ()
-    ((_ ((vari vali) ...) body)
-     (match-let* ((vari (auto-list1 vali)) ...) (auto-list1 body)))
-    ((_ (((vari1 vari2) vali) ...) body)
-     (match-let* (((vari1 vari2) (auto-list1 vali)) ...) (auto-list1 body)))))
-
-;; SYNTACTIC CONSTRUCTS FOR CHECKING VARIABLES
-
-(define-syntax Case
-  (syntax-rules (else)
-    ((_ var ((pati bodi) ...))
-     (handle-exceptions exn ((amb-failure-continuation))
-                        (match (auto-list1 var) (pati (auto-list1 bodi)) ...)))))
-
-(define-syntax If
-  (syntax-rules ()
-    ((_ condition thenbody elsebody)
-        (if condition (auto-list1 thenbody) (auto-list1 elsebody)))
-    ((_ condition thenbody)
-        (if condition (auto-list1 thenbody) ((amb-failure-continuation))))))
-
-;; QUERY STATEMENT EXECUTION OPERATOR
-
+;; Query statement, execution operator, prining the elements
 (define (print-solutions xs)
   (display "[")
   (define (print-items xs)
@@ -91,8 +44,142 @@
     ((_ argi ...)
      (print-solutions (amb-collect (auto-list argi ...))))))
 
-;; AUTO-LIST TO ELIMINATE THE NEED FOR LIST-FUNCTIONCALL DISTINCTION
 
+;; Collapse: using Scheme's 'amb-collect' implements MeTTa's collapse
+(define-syntax collapse
+  (syntax-rules ()
+    ((_ args)
+     (filter (lambda (x) (not (== x (if #f 42))))
+             (amb-collect (handle-exceptions exn ((amb-failure-continuation)) args))))))
+
+;; Superpose: using Scheme's ambivalence operator 'amb1' implements MeTTa's superpose.
+;;            superpose-helper is for MeTTa's nested superpose statements
+(define-syntax superpose
+  (syntax-rules ()
+    ((_ args)
+     (amb1 (superpose-helper args)))))
+
+(define-syntax superpose-helper
+  (syntax-rules ()
+    ((_ ((superpose x) ...))
+     (amb x ...))
+    ((_ arg)
+     (auto-list1 arg))))
+
+;; MeTTa function definition: Creates a hash map where the functions with all possible 
+;; combinations of arguments are registered as keys. The values are the lists containing 
+;; function's bodies to allow functions with the same definition to appear more than once
+(define functions (make-hash-table)) ; global function variable
+
+(define-syntax =
+  (syntax-rules ()
+    ((_ (name patterni ...) body)
+     (begin
+       (hash-table-set! vars '&self (cons '(=def (name patterni ...) body) (hash-table-ref vars '&self)))
+       (let ((name (match-lambda* ((patterni ...) (auto-list1 body)))))
+            (if (hash-table-exists? functions 'name)
+                (hash-table-set! functions 'name (cons name (hash-table-ref functions 'name)))
+                (hash-table-set! functions 'name (list name))))
+       (set! name (lambda args (handle-exceptions exn ((amb-failure-continuation))
+                          (apply (amb1 (hash-table-ref functions 'name)) args))))))))
+
+
+;; MeTTa's Let and Let*: Using Scheme's match-let*
+(define-syntax Let
+  (syntax-rules ()
+    ((_ var val body)
+     (match-let* ((var (auto-list1 val))) (auto-list1 body)))))
+
+(define-syntax Let*
+  (syntax-rules ()
+    ((_ ((vari vali) ...) body)
+     (match-let* ((vari (auto-list1 vali)) ...) (auto-list1 body)))
+    ((_ (((vari1 vari2) vali) ...) body)
+     (match-let* (((vari1 vari2) (auto-list1 vali)) ...) (auto-list1 body)))))
+
+
+;; MeTTa's case expression 
+(define-syntax Case
+  (syntax-rules (else)
+    ((_ var ((pati bodi) ...))
+     (handle-exceptions exn ((amb-failure-continuation))
+                        (match (auto-list1 var) (pati (auto-list1 bodi)) ...)))))
+
+
+;; Defined in NARS Utils as alternative 'if' statement
+(define-syntax If
+  (syntax-rules ()
+    ((_ condition thenbody elsebody)
+        (if condition (auto-list1 thenbody) (auto-list1 elsebody)))
+    ((_ condition thenbody)
+        (if condition (auto-list1 thenbody) ((amb-failure-continuation))))))
+
+
+;; Definition of equality to allow using '==' for MeTTa compatibility
+(define == equal?)
+
+;; Ttrace 
+(define (trace! x y) (begin (display x) y))
+
+;; SPACES IMPLEMENTATION
+
+(define vars (make-hash-table))
+(hash-table-set! vars '&self '())
+(define (new-space S) S)
+(define (new-state S) S)
+(define (get-state S) S)
+(define (get-atoms S) (amb1 (hash-table-ref vars S)))
+
+(define (add-atom space atom)
+  (begin (hash-table-set! vars space (cons atom (hash-table-ref vars space))) '()))
+
+(define (remove-atom space atom)
+  (begin (hash-table-set! vars space (delete atom (hash-table-ref vars space))) '()))
+
+(define (bind! var val)
+  (begin (hash-table-set! vars var val) '()))
+
+(define (change-state! var val)
+  (begin (hash-table-set! vars var val) (list 'State val)))
+
+(define (get-state var)
+  (hash-table-ref vars var))
+
+
+;; MeTTa match expression: using Scheme's match-let* 
+(define-syntax Match
+  (syntax-rules (MatchChain)
+    ((_ space (MatchChain bind1 bind2) result)
+     (Match space bind1 (Match space bind2 result)))
+    ((_ space (MatchChain bind1 bindi ...) result)
+     (Match space bind1 (Match space (MatchChain bindi ...) result)))
+    ((_ space binds result)
+     (handle-exceptions exn ((amb-failure-continuation))
+                        (match-let* ((binds (amb1 (hash-table-ref vars space)))) (auto-list1 result))))))
+
+
+;; For procudural (sequential) constructs: in Scheme sequential cannot be
+;; superpose as in MeTTa. Procedural sequential execution demands ':begin' 
+(define-syntax sequential-helper
+  (syntax-rules (do)
+    ((_ (do expr))
+     expr)
+    ((_ expr)
+     (set! ret (append ret (list expr))))))
+
+(define-syntax sequential ;sequential cannot be superpose in Scheme as in MeTTa
+  (syntax-rules ()        ;as procedural sequential execution demands :begin"
+    ((_ (expri ...))      ;that's why this construct is defined here instead
+     (begin
+       (set! ret '())
+       (sequential-helper (auto-list1 expri)) ...
+       (amb1 ret)))))
+
+
+;; AUTO-LIST to eliminate the need for distinguishing LIST from FUNCTION call
+;; Returns a list if arg is a list or executes a function if it is a function
+;; To avoid expansion of a full algorithmic tree and make compiling efficient
+;; MeTTa macros definition is used to limit expansion only to defined macros
 (define-syntax metta-macro-if
   (syntax-rules (collapse superpose Let Let* Match Case If == sequential quote)
     ((_ collapse then else) then)
@@ -133,80 +220,3 @@
      (auto-list vari ...))
     ((_ var1)
      var1)))
-
-;; EQUALITY
-
-(define == equal?)
-
-;; TYPE SYSTEM
-
-(define-type Atom *)
-(define-type Symbol symbol)
-(define-type Expression list)
-(define-type Bool boolean)
-(define-type Number number)
-(define-type String string)
-
-(define-syntax Typedef
-  (syntax-rules ()
-    ((_ arg (-> A ... B))
-     (cond-expand
-       (USE_TYPES (: arg (A ... -> B))) (else '())))
-    ((_ arg1 arg2)
-     (cond-expand
-       (USE_TYPES (define-type arg1 arg2)) (else '())))))
-
-;; SPACES IMPLEMENTATION
-
-(define vars (make-hash-table))
-(hash-table-set! vars '&self '())
-(define (new-space S) S)
-(define (new-state S) S)
-(define (get-state S) S)
-(define (get-atoms S) (amb1 (hash-table-ref vars S)))
-
-(define (add-atom space atom)
-  (begin (hash-table-set! vars space (cons atom (hash-table-ref vars space))) '()))
-
-(define (remove-atom space atom)
-  (begin (hash-table-set! vars space (delete atom (hash-table-ref vars space))) '()))
-
-(define (bind! var val)
-  (begin (hash-table-set! vars var val) '()))
-
-(define (change-state! var val)
-  (begin (hash-table-set! vars var val) (list 'State val)))
-
-(define (get-state var)
-  (hash-table-ref vars var))
-
-(define-syntax Match
-  (syntax-rules (MatchChain)
-    ((_ space (MatchChain bind1 bind2) result)
-     (Match space bind1 (Match space bind2 result)))
-    ((_ space (MatchChain bind1 bindi ...) result)
-     (Match space bind1 (Match space (MatchChain bindi ...) result)))
-    ((_ space binds result)
-     (handle-exceptions exn ((amb-failure-continuation))
-                        (match-let* ((binds (amb1 (hash-table-ref vars space)))) (auto-list1 result))))))
-
-;; PROCEDURAL CONSTRUCTS
-
-(define-syntax sequential-helper
-  (syntax-rules (do)
-    ((_ (do expr))
-     expr)
-    ((_ expr)
-     (set! ret (append ret (list expr))))))
-
-(define-syntax sequential ;sequential cannot be superpose in Scheme as in MeTTa
-  (syntax-rules ()        ;as procedural sequential execution demands :begin"
-    ((_ (expri ...))      ;that's why this construct is defined here instead
-     (begin
-       (set! ret '())
-       (sequential-helper (auto-list1 expri)) ...
-       (amb1 ret)))))
-
-;; TRACE
-
-(define (trace! x y) (begin (display x) y))
