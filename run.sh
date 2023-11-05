60b5e4387c006f18ace5df6428d14065331b22a4
Patrick Hammer <patham9@gmail.com>
2023-11-05 17:08:50 +0100

    Update: utilize the temp folder for the intermediate steps

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
-time sh runmetta.sh $1 > OUTPUT_METTA
-cat OUTPUT_METTA
-time sh runscheme.sh $1 > OUTPUT_SCHEME
-cat OUTPUT_SCHEME
-(cmp OUTPUT_METTA OUTPUT_SCHEME > /dev/null && echo "==") || echo "!="
+(cd ./temp && cd ..) || mkdir temp
+time sh runmetta.sh $1 > ./temp/OUTPUT_METTA
+cat ./temp/OUTPUT_METTA
+time sh runscheme.sh $1 > ./temp/OUTPUT_SCHEME
+cat ./temp/OUTPUT_SCHEME
+(cmp ./temp/OUTPUT_METTA ./temp/OUTPUT_SCHEME > /dev/null && echo "==") || echo "!="
--- runmetta.sh
+++ runmetta.sh
@@ -1,6 +1,6 @@
-awk '{gsub(/\(list /, "(") gsub(/\047/, ""); if($0 ~ /^[(]/ && !($0 ~ /^[(][:|=]/)) { gsub(/^[(]/, "!(add-atom \\&self (", $0); $0 = $0 ")" }}1' $1 > PROGRAM.metta
-cat mettamorph.metta PROGRAM.metta > RUN.metta
 (cd ./temp && cd ..) || mkdir temp
+awk '{gsub(/\(list /, "(") gsub(/\047/, ""); if($0 ~ /^[(]/ && !($0 ~ /^[(][:|=]/)) { gsub(/^[(]/, "!(add-atom \\&self (", $0); $0 = $0 ")" }}1' $1 > ./temp/PROGRAM.metta
+cat mettamorph.metta ./temp/PROGRAM.metta > RUN.metta
 echo "cat-only" > ./temp/PARAM2_SHOULD
 echo "$2" > ./temp/PARAM2_IS
 cmp -s ./temp/PARAM2_IS ./temp/PARAM2_SHOULD || metta RUN.metta
