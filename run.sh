a7ccc04ac173db0d6cb43c5d3d9160be972aaa36
Patrick Hammer <patham9@gmail.com>
2023-11-06 17:49:52 +0100

    Update: omit non-portable version of time usage

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 (cd ./temp && cd ..) || mkdir temp
-time sh run_metta.sh $1 > ./temp/OUTPUT_METTA
+sh run_metta.sh $1 > ./temp/OUTPUT_METTA
 cat ./temp/OUTPUT_METTA
-time sh run_scheme.sh $1 > ./temp/OUTPUT_SCHEME
+sh run_scheme.sh $1 > ./temp/OUTPUT_SCHEME
 cat ./temp/OUTPUT_SCHEME
 (cmp ./temp/OUTPUT_METTA ./temp/OUTPUT_SCHEME > /dev/null && echo "==") || echo "!="
