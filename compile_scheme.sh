f25dcf1099b7f67adf60a632291b2d5bab85bf8d
Patrick Hammer <patham9@gmail.com>
2023-11-06 16:20:10 +0100

    Update: more words on compiler flags

--- compile_scheme.sh
+++ compile_scheme.sh
@@ -1,5 +1,8 @@
 echo "Compilation started."
-#Flags="-O5 -strict-types -lfa2" can cause issues for repl
 Flags=""
+#Uncomment the following if you want to maximize performance if you do not mind waiting for a few minutes to end up with a heavily-optimized binary!
+#In the following O5 flags are mentioned with two differences: -strict-types is used which is not in O5 but for Mettamorph is a valid assumption.
+#-block is not used as it would not allow to call code from the repl, if that doesn't matter you can add it to the list as well:
+#Flags="-strict-types -optimize-leaf-routines -inline -lfa2 -inline-global -specialize -unsafe -disable-interrupts -no-trace -no-lambda-info -clustering"
 csc $Flags -DUSE_TYPES RUN.scm || (echo "\nERROR IN TYPE DEFINITIONS ENCOUNTERED!! COMPILING WITHOUT TYPE INFO NOW..." && csc $Flags RUN.scm)
 echo "Compilation complete."
