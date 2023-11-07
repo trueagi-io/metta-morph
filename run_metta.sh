0c2bd0b9f036619138cbac7147bfa085cf8c98fe
Patrick Hammer <patham9@gmail.com>
2023-11-07 21:08:36 +0100

    Update: omit temp output also in run_metta.sh

--- run_metta.sh
+++ run_metta.sh
@@ -1,5 +1,5 @@
 (cd ./temp && cd ..) || mkdir temp
-cp $1 ./temp/CODE.metta #copy for repl only
+cp $1 ./temp/CODE.metta > /dev/null 2>&1 #copy for repl only
 awk '{gsub(/\(list /, "(") gsub(/\047/, ""); if($0 ~ /^[(]/ && !($0 ~ /^[(][:|=]/)) { gsub(/^[(]/, "!(add-atom \\&self (", $0); $0 = $0 ")" }}1' $1 > ./temp/PROGRAM.metta
 cat mettamorph.metta ./temp/PROGRAM.metta > RUN.metta
 echo "cat-only" > ./temp/PARAM2_SHOULD
